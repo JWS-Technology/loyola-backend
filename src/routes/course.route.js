20806906b55b42c18b160295dfd34db60ca78d1d
Joerakesh <rakeshjoe52@gmail.com>
2025-11-04 23:02:50 +0530

    chore: course route

---
+++ src/routes/course.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+    createCourse,
+    getAllCourses,
+    getCourseById,
+    updateCourse,
+    deleteCourse,
+} from "../controllers/course.controller.js";
+
+const router = express.Router();
+
+// CRUD Endpoints
+router.post("/", createCourse);
+router.get("/", getAllCourses);
+router.get("/:id", getCourseById);
+router.put("/:id", updateCourse);
+router.delete("/:id", deleteCourse);
+
+export default router;
