ccca50e52c1e565b77556f89bc1327158eafd2d2
Joerakesh <rakeshjoe52@gmail.com>
2025-11-18 21:57:19 +0530

    updated server file with all routes

--- src/server.js
+++ src/server.js
@@ -2,23 +2,38 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./utils/db.js"; // Import MongoDB connection
+import cookieParser from "cookie-parser";
 
 import studentRoutes from "./routes/student.route.js";
 import attendanceRoutes from "./routes/attendance.route.js";
 import subjectRoutes from "./routes/subject.route.js";
 import courseRoutes from "./routes/course.route.js";
-
+import timetableRoutes from "./routes/timetable.route.js";
+import staffRoutes from "./routes/staff.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:3000",
+    // "http://10.147.87.165:3000",
+];
+
+app.use(
+    cors({
+        origin: allowedOrigins,
+        credentials: true,
+    })
+);
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // Connect Database
 connectDB();
 
+app.use(cookieParser());
 
 // Test route
 app.get("/", (req, res) => {
@@ -29,6 +44,10 @@ app.use("/api/students", studentRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/subjects", subjectRoutes);
 app.use("/api/courses", courseRoutes);
+app.use("/api/timetable", timetableRoutes);
+app.use("/api/staff", staffRoutes);
+
+app.use("/api/auth", authRoutes);
 
 // Server
 const PORT = process.env.PORT || 5000;
