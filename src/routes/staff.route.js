ffd4a22f4855fc9470c771c4c1e30afb4f000076
Joerakesh <rakeshjoe52@gmail.com>
2025-11-20 21:57:05 +0530

    added route

--- src/routes/staff.route.js
+++ src/routes/staff.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createStaff, getAllStaff } from "../controllers/staff.controller.js";
+import { createStaff, getAllStaff, getStaffById } from "../controllers/staff.controller.js";
 
 const router = express.Router();
 
 router.post("/", createStaff);
 router.get("/", getAllStaff);
+router.get("/:id", getStaffById);
 
 export default router;
